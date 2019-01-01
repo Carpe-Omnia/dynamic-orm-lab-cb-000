4ff9eadcf5e0190851bffca9c4bc11c3f651981d
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-01 22:54:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  self.class.column_names.each do |col_name|
+  self.column_names.each do |col_name|
     values << "'#{send(col_name)}'" unless send(col_name).nil?
   end
 end
