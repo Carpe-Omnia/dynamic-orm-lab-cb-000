3db105076067939e1911d7709c9df74212856df5
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-01 22:57:37 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,8 +51,8 @@ class InteractiveRecord
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
-  def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+  def self.find_by(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name[:name]}'"
     DB[:conn].execute(sql)
   end
 end
